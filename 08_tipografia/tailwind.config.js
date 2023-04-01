b38180ce0725501e9fa2b3e3130707c1dd897ba4
weslleypmfortunato <111170704+weslleypmfortunato@users.noreply.github.com>
2023-03-31 17:19:51 -0700

    exercise 13

---
+++ 08_tipografia/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'arial': 'Arial'
+      },
+      fontSize: {
+        '12xl': '10rem'
+      },
+      lineHeight: {
+        '12xl': 1
+      }
+    },
+  },
+  plugins: [],
+}
+
