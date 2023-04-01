be6ce8fa9e51eb0882ac938fddf4772f55239de7
weslleypmfortunato <111170704+weslleypmfortunato@users.noreply.github.com>
2023-03-31 20:29:08 -0700

    final background

---
+++ 09_background/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'victoria': "url('../src/images/background-img.jpeg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
